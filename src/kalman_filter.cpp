0f134e6b98797454f7e0fa33b6b2ae048613ce50
Sundeep Pundamale-Selvaraj <sundeep.selvaraj@here.com>
2018-01-30 08:21:11 +0100

    Bug fix in "UpdateEKF method"

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -58,28 +58,32 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
   TODO:
     * update the state by using Extended Kalman Filter equations
-  */
-  float px = x_(0);
-  float py = x_(1);
-  float vx = x_(2);
-  float vy = x_(3);
+   **/
+    // Update
+    float px = x_(0);
+    float py = x_(1);
+    float vx = x_(2);
+    float vy = x_(3);
 
-  float rho = sqrt(px * px + py * py);
-  float theta = atan2(py, px);
-  float ro_dot = (px * vx + py * vy)/rho;
-  VectorXd z_pred = VectorXd(3);
-  z_pred << rho, theta, ro_dot;
+    float rho = sqrt(px * px + py * py);
+    float theta = atan2(py, px);
+    float ro_dot = (px * vx + py * vy)/rho;
+    VectorXd z_pred = VectorXd(3);
+    z_pred << rho, theta, ro_dot;
 
-  VectorXd y = z - z_pred;
+    VectorXd y = z - z_pred;
 
-  MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
-  MatrixXd K =  P_ * Ht * Si;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd K =  P_ * Ht * Si;
 
-  //KF Prediction step
-  x_ = x_ + ( K * y );
-  MatrixXd Ft = F_.transpose();
-  P_ = F_ * P_ * Ft + Q_;
+    x_ = x_ + ( K * y );
+
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = ( I - K * H_ ) * P_;
+
+
 
 }
